02b9f452028bfe460614a8613d407d1fbbdb41ec
USER <USER@AMP-10>
2019-04-25 19:26:53 +0900

    commit

---
+++ practice_2019-04-25/src/Various.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Various extends JFrame{
+
+	public static void main(String[] args) {
+		new Various();
+
+	}
+
+}
