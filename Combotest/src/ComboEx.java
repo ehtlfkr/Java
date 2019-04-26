f5b76b073df510056dfb7c32e7a6ffe51afdc9d4
USER <USER@AMP-10>
2019-04-26 19:45:32 +0900

    commit

--- Combotest/src/ComboEx.java
+++ Combotest/src/ComboEx.java
@@ -3,13 +3,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class ComboEx extends JFrame {
-	private String [] fruits = { "apple", "inkyung1", "inkyung2", "inkyung3", "inkyung4" };
+	private String [] fruits = { "apple", "inkyung1", "inkyung2", "inkyung3", "inkyung4", "inkyung5" };
 	private ImageIcon [] images = {
 			new ImageIcon("images/apple.jpg"),
 			new ImageIcon("images/inkyung1.jpg"),
 			new ImageIcon("images/inkyung2.jpg"),
 			new ImageIcon("images/inkyung3.jpg"),
-			new ImageIcon("images/inkyung4.jpg")};
+			new ImageIcon("images/inkyung4.jpg"),
+			new ImageIcon("images/inkyung5.jpg")};
 	private JLabel imgLabel = new JLabel(images[0]);
 	private JComboBox<String> strCombo = new JComboBox<String>(fruits);
 	
@@ -23,9 +24,16 @@ public class ComboEx extends JFrame {
 		c.add(imgLabel);
 		
 		strCombo.addActionListener(new ActionListener() {
+			private int width, height;
+			
 			public void actionPerformed(ActionEvent e) {
 				JComboBox<String> cb = (JComboBox<String>)e.getSource();
 				int index = cb.getSelectedIndex();
+				/*for(int i = 0; i<images.length; i++){
+					width = images[i].getIconWidth();
+					height = images[i].getIconHeight();
+				}*/
+				
 				imgLabel.setIcon(images[index]);
 			}
 		});
