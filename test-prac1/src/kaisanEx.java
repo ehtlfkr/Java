9adacc75c8b72d104f3d363861f356ed59f293da
USER <USER@AMP-09>
2019-05-20 16:33:39 +0900

    commit

--- test-prac1/src/kaisanEx.java
+++ test-prac1/src/kaisanEx.java
@@ -37,15 +37,16 @@ public class kaisanEx extends JFrame{
 	
 	int number1 = 10;
 	int number2 = 10;
-	int num1 = 0;
-	int num2 = 0;
-	int num3 = 0;
-	int num4 = 0;
+	int num1;
+	int num2;
+	int num3;
+	int num4;
 	String tt;
 	String numt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
 	String tt2[] = { "+", "-", "*", "/", "%" };
 	
-	private JTextArea text = new JTextArea(2, 25);
+	JTextArea text = new JTextArea(2, 20);
+
 	
 	public kaisanEx(){
 		setTitle("계산기");
@@ -53,8 +54,10 @@ public class kaisanEx extends JFrame{
 	
 		Container cc = getContentPane();
 		cc.setLayout(new FlowLayout());
+		text.setFont(new Font("asklashfas", Font.PLAIN, 15));
 		
 		cc.add(text);
+		
 		
 		for(int i = 0; i < numt.length; i++){
 			JButton njb = new JButton(numt[i]);
@@ -62,7 +65,7 @@ public class kaisanEx extends JFrame{
 			
 			njb.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					JButton b = (JButton)e.getSource();
+					JButton b = (JButton)e.getSource(); 
 					String a = b.getText();
 					text.append(a);
 					
@@ -97,112 +100,119 @@ public class kaisanEx extends JFrame{
 		
 		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(tt == null)
-					text.setText("");
-				else {
-					switch (tt) {
-						case "+":
-							dh dh = new dh();
-							int nan1 = dh.dhtest(num1, num2);
+				int nan;
+				switch (tt) {
+					case "+":
+						dh dh = new dh();
+						nan = dh.dhtest(num1, num2);
+						text.setText("");
+						text.append(String.valueOf(nan));
+						num1 = nan;
+						num2 = 0;
+						number1 = 1;
+						number2 = 1;
+						tt = null;
+						
+						break;
+						
+					case "-":
+						bg bg = new bg();
+						nan = bg.bgtest(num1, num2);
+						text.setText("");
+						text.append(String.valueOf(nan));
+						num1 = nan;
+						num2 = 0;
+						number1 = 1;
+						number2 = 1;
+						tt = null;
+						
+						break;
+						
+					case "*":
+						gh gh = new gh();
+						nan = gh.ghtest(num1, num2);
+						text.setText("");
+						text.append(String.valueOf(nan));
+						num1 = nan;
+						num2 = 0;
+						number1 = 1;
+						number2 = 1;
+						tt = null;
+						
+						break;
+						
+					case "/":
+						nn nn = new nn();
+						if(num1 < num2){
 							text.setText("");
-							text.append(String.valueOf(nan1));
-							num1 = nan1;
+							text.append("나누는 수가 더 크면 나눌 수 없습니다.");
+							tt = null;
+						}
+						
+						else if(num2 != 0){
+							nan = nn.nntest(num1, num2);
+							text.setText("");
+							text.append(String.valueOf(nan));
+							num1 = nan;
 							num2 = 0;
 							number1 = 1;
 							number2 = 1;
 							tt = null;
-							
-							break;
-							
-						case "-":
-							bg bg = new bg();
-							int nan2 = bg.bgtest(num1, num2);
+						}
+						
+						else if(num2 == 0){
 							text.setText("");
-							text.append(String.valueOf(nan2));
-							num1 = nan2;
+							text.append("0으론 나눌 수 없습니다.");
+							tt = null;
+						}
+						
+						break;
+						
+					case "%":
+						nm nm = new nm();
+						if(num1 < num2){
+							text.setText("");
+							text.append("나누는 수가 더 크면 나눌 수 없습니다.");
+							tt = null;
+						}
+						
+						else if(num2 != 0){
+							nan = nm.nmtest(num1, num2);
+							text.setText("");
+							text.append(String.valueOf(nan));
+							num1 = nan;
 							num2 = 0;
 							number1 = 1;
 							number2 = 1;
 							tt = null;
-							
-							break;
-							
-						case "*":
-							gh gh = new gh();
-							int nan3 = gh.ghtest(num1, num2);
+						}
+						
+						else if(num2 == 0){
 							text.setText("");
-							text.append(String.valueOf(nan3));
-							num1 = nan3;
-							num2 = 0;
-							number1 = 1;
-							number2 = 1;
+							text.append("0으론 나눌 수 없습니다.");
 							tt = null;
-							
-							break;
-							
-						case "/":
-							nn nn = new nn();
-							int nan4;
-							if(num1 < num2){
-								text.setText("");
-								text.append("나누는 수가 더 크면 나눌 수 없습니다.");
-								tt = null;
-							}
-							
-							else if(num2 != 0){
-								nan4 = nn.nntest(num1, num2);
-								text.setText("");
-								text.append(String.valueOf(nan4));
-								num1 = nan4;
-								num2 = 0;
-								number1 = 1;
-								number2 = 1;
-								tt = null;
-							}
-							
-							else if(num2 == 0){
-								text.setText("");
-								text.append("0으론 나눌 수 없습니다.");
-								tt = null;
-							}
-							
-							break;
-							
-						case "%":
-							nm nm = new nm();
-							int nan5;
-							
-							if(num1 < num2){
-								text.setText("");
-								text.append("나누는 수가 더 크면 나눌 수 없습니다.");
-								tt = null;
-							}
-							
-							else if(num2 != 0){
-								nan5 = nm.nmtest(num1, num2);
-								text.setText("");
-								text.append(String.valueOf(nan5));
-								num1 = nan5;
-								num2 = 0;
-								number1 = 1;
-								number2 = 1;
-								tt = null;
-							}
-							
-							else if(num2 == 0){
-								text.setText("");
-								text.append("0으론 나눌 수 없습니다.");
-								tt = null;
-							}
-							
-							break;	
-		
-						default:	
-					}
+						}
+						
+						break;
+	
+					default:
 				}
 			}
 		});
 		
+		JButton clear = new JButton("C");
+		cc.add(clear);
+		
+		clear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				text.setText("");
+				num1 = 0;
+				num2 = 0;
+				num3 = 0;
+				num4 = 0;
+				tt = null;
+			}
+		});
 		
 		setSize(300,400);
 		setVisible(true);
